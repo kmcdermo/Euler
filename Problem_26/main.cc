616175e6c49974c30e75cb76c8ed57cc03cc524f
Kevin McDermott <kpm82@cornell.edu>
2017-08-14 12:29:50 +0200

    test the reoccurance of the REMAINDERS, not the string pattern itself

--- Problem_26/main.cc
+++ Problem_26/main.cc
@@ -2,26 +2,17 @@
 
 static const int N = 1000;
 
-int checkRepeat(const TString & decimal, bool& isRepeat)
+int checkRepeat(const std::vector<int>& remainders, const int remainder)
 {
-  //  std::cout << "START: " << decimal.Data() << std::endl;
-
   int length = 0;
-  for (int i = 0; i < decimal.Length(); i++)
+  for (int i = 0; i < remainders.size(); i++)
   {
-    if (i > 1) break;
-    for (int j = 1; j <= (decimal.Length()-i)/2 ; j++)
+    if (remainder == remainders[i])
     {
-      TString lhs(decimal(i,j));
-      TString rhs(decimal(i+j,j));
-
-      //  std::cout << "i: " << i << " j: " << j << " lhs: " << lhs.Data() << " rhs: " << rhs.Data() << std::endl; 
-
-      if (lhs == rhs) {isRepeat = true; length = j; break;}
+      length = remainders.size()-i;
+      break;
     }
-    if (isRepeat) break;
   }
-
 
   return length;
 }
@@ -30,37 +21,40 @@ int getRecurringCycle(const int dividend)
 {
   bool isOver = false;
   int divisor = 1;
-  TString decimal = "";
-  int length = decimal.Length();
+  std::vector<int> remainders; 
+  int length = 0;
   while (!isOver)
   {
     const int quotient = divisor / dividend;
-    //    std::cout << divisor << " / " << dividend << " = " << quotient << std::endl;
+    const int remainder = divisor % dividend;
     if (quotient == 0)
     {
-      divisor *= 10;
-      if (decimal.Length() != 0) decimal.Append(Form("%i",0));
-      length = checkRepeat(remainders,divisor,isOver);
+      length = checkRepeat(remainders,remainder);
+      if (length > 0) isOver = true;
+      if (!isOver)
+      {
+	remainders.push_back(remainder);
+	divisor *= 10;
+      }
     }
     else
     {
-      const int remainder = divisor % dividend;
-      //std::cout << divisor << " % " << dividend << " = " << remainder << std::endl;
       if (remainder == 0)
       {
 	isOver = true;
       }
       else
       {
-	divisor = remainder * 10;
-	decimal.Append(Form("%i",quotient));
-	length = checkRepeat(decimal,isOver);
+	length = checkRepeat(remainders,remainder);
+	if (length > 0) isOver = true;
+	if (!isOver)
+	{
+	  remainders.push_back(remainder);
+	  divisor = remainder * 10;
+	}
       }
     }
-    //    std::cout << decimal.Data() << std::endl << std::endl;
   }
-
-  //  std::cout << dividend << " " << decimal.Data() << std::endl;
   return length;
 }
 
@@ -69,9 +63,7 @@ int main()
   int d = 0, length = 0;
   for (int i = 2; i < N; i++)
   {
-    //    std::cout << "I: "<< i << std::endl;
     int tmp = getRecurringCycle(i);
-    //    std::cout << "length for " << i << " : " << tmp << std::endl << std::endl; 
     if (tmp > length) {length = tmp; d = i;}
   }
   std::cout << d << std::endl;
